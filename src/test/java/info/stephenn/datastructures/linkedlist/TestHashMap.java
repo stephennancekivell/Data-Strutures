32720249b5c9a2320c70bc2fa770235ac29f67f2
Stephen Nancekivell <stephen.nancekivell@gmail.com>
2012-04-13 23:49:46 +1200

    tests for KDtree

--- src/test/java/info/stephenn/datastructures/linkedlist/TestHashMap.java
+++ src/test/java/info/stephenn/datastructures/linkedlist/TestHashMap.java
@@ -2,6 +2,13 @@ package info.stephenn.datastructures.linkedlist;
 
 import static org.junit.Assert.*;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 import org.junit.Test;
 
 public class TestHashMap {
@@ -53,17 +60,55 @@ public class TestHashMap {
 	public void profile1(){
 		HashMap<String,Integer> map = new HashMap<String,Integer>();
 		
-		long toPutIn = 10000000;
-		long start = System.currentTimeMillis();
+		int increment = 1000000;
+		int count = 0;
 		
-		for (int i=0; i < toPutIn; i++){
+		for (int i=0 ; i < 50; i++){
+			profileSome(increment,count,map);
+			count += increment;
+		}
+	}
+	
+	private void profileSome(int toPut, int putFrom, HashMap<String,Integer> map){
+		long start = System.currentTimeMillis();
+		for (int i=putFrom; i < toPut+putFrom; i++){
 			map.put(Integer.toOctalString(i), i);
 		}
-		
 		long end = System.currentTimeMillis();
+		float insertsPerMilliSecond = toPut / (end-start);
+		System.out.println("inserts pr milli "+ insertsPerMilliSecond + " got to size "+map.getEntryCount() +" over "+(end-start)+"ms occupancy "+map.getOccupancy()+" collisions"+map.getCollisions());
+	}
+	
+	@Test
+	public void profile2() throws FileNotFoundException{
+		HashMap<String,Integer> map = new HashMap<String,Integer>();
+		Integer[] data = loadData();
 		
-		float insertsPerMilliSecond = toPutIn / (end-start);
+		int increment = 1000000;
+		int count = 0;
 		
-		System.out.println("inserts pr milli "+ insertsPerMilliSecond + " got to size "+map.getEntryCount() +" over "+(end-start)+"ms");
+		for (int i=0 ; i < 50; i++){
+			profileSomeFrom(data,count,increment+count,map);
+			count += increment;
+		}
+	}
+	
+	private void profileSomeFrom(Integer[] data, int from, int to, HashMap<String,Integer> map){
+		long start = System.currentTimeMillis();
+		for(int i=from; i < to; i++){
+			map.put(Integer.toString(data[i].hashCode()), i);
+		}
+		long end = System.currentTimeMillis();
+		float insertsPerMilliSecond = (to-from) / (end-start);
+		System.out.println("inserts pr milli "+ insertsPerMilliSecond + " got to size "+map.getEntryCount() +" over "+(end-start)+"ms occupancy "+map.getOccupancy()+" collisions"+map.getCollisions());
+	}
+	
+	private Integer[] loadData() throws FileNotFoundException{
+		Scanner sc = new Scanner(new FileInputStream( "key.txt"));
+		List<Integer> list = new ArrayList<Integer>();
+		while (sc.hasNext()){
+			list.add(sc.nextInt());
+		}
+		return list.toArray(new Integer[0]);
 	}
 }
