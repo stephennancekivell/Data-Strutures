1657affd6ba46c4d74f10784304feef880086f2a
Stephen Nancekivell <stephen.nancekivell@gmail.com>
2012-04-19 20:48:28 +1200

    nearest neigbour

--- src/test/java/info/stephenn/datastructures/TestKDTree.java
+++ src/test/java/info/stephenn/datastructures/TestKDTree.java
@@ -2,10 +2,9 @@ package info.stephenn.datastructures;
 
 import static org.junit.Assert.*;
 import info.stephenn.datastructures.KDTree.Node;
+import info.stephenn.datastructures.KDTree.Node.Point;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 import org.junit.Test;
@@ -14,94 +13,127 @@ public class TestKDTree {
 	
 	@Test
 	public void canCreate(){
-		List<List<Integer>> list = new ArrayList<List<Integer>>();
-		list.add(Arrays.asList(2, 1));
-		list.add(Arrays.asList(1, 1));
-		list.add(Arrays.asList(3, 3));
+		List<Point> list = new ArrayList<Point>();
+		list.add(new Point(new int[]{2,1}));
+		list.add(new Point(new int[]{1,1}));
+		list.add(new Point(new int[]{3,3}));
 		
 		KDTree.Node head = KDTree.build(list, 0);
 		
-		assertEquals(new Integer(2), head.keys.get(0));
-		assertEquals(new Integer(1), head.keys.get(1));
-		assertEquals(new Integer(1), head.left.keys.get(0));
-		assertEquals(new Integer(1), head.left.keys.get(1));
-		assertEquals(new Integer(3), head.right.keys.get(0));
-		assertEquals(new Integer(3), head.right.keys.get(1));
+		assertEquals(0, head.axis);
+		assertEquals(1, head.left.axis);
+		assertEquals(1, head.right.axis);
+		assertArrayEquals(new int[] {2,1}, head.point.values);
+		assertArrayEquals(new int[] {1,1}, head.left.point.values);
+		assertArrayEquals(new int[] {3,3}, head.right.point.values);
 	
 		//order doesnt matter
 		list.add(list.remove(1));
 		head = KDTree.build(list, 0);
 		
-		assertEquals(new Integer(2), head.keys.get(0));
-		assertEquals(new Integer(1), head.keys.get(1));
 		assertEquals(0, head.axis);
-		assertEquals(new Integer(1), head.left.keys.get(0));
-		assertEquals(new Integer(1), head.left.keys.get(1));
 		assertEquals(1, head.left.axis);
-		assertEquals(new Integer(3), head.right.keys.get(0));
-		assertEquals(new Integer(3), head.right.keys.get(1));
 		assertEquals(1, head.right.axis);
+		assertArrayEquals(new int[] {2,1}, head.point.values);
+		assertArrayEquals(new int[] {1,1}, head.left.point.values);
+		assertArrayEquals(new int[] {3,3}, head.right.point.values);
 	}
 	
 	@Test
 	public void canLookup(){
-		List<List<Integer>> list = new ArrayList<List<Integer>>();
-		list.add(Arrays.asList(2, 1));
-		list.add(Arrays.asList(1, 1));
-		list.add(Arrays.asList(3, 3));
+		List<Point> list = new ArrayList<Point>();
+		list.add(new Point(new int[]{2,1}));
+		list.add(new Point(new int[]{1,1}));
+		list.add(new Point(new int[]{3,3}));
 		
-		KDTree.Node head = KDTree.build(list, 0);
+		Node head = KDTree.build(list, 0);
 		
-		KDTree.Node result = head.getChild(Arrays.asList(2,1));
+		KDTree.Node result = head.getChild(new Point(new int[]{2,1}));
 		assertNotNull(result);
-		assertEquals(new Integer(2),result.keys.get(0));
-		assertEquals(new Integer(1),result.keys.get(1));
+		assertArrayEquals(new int[]{2,1}, result.point.values);
 		
-		result = head.getChild(Arrays.asList(3,3));
+		result = head.getChild(new Point(new int[] {3,3}));
 		assertNotNull(result);
-		assertEquals(new Integer(3),result.keys.get(0));
-		assertEquals(new Integer(3),result.keys.get(1));
+		assertArrayEquals(new int[]{3,3}, result.point.values);
 		
-		result = head.getChild(Arrays.asList(1,1));
+		result = head.getChild(new Point(new int[]{1,1}));
 		assertNotNull(result);
-		assertEquals(new Integer(1),result.keys.get(0));
-		assertEquals(new Integer(1),result.keys.get(1));
+		assertArrayEquals(new int[]{1,1}, result.point.values);
 		
-		result = head.getChild(Arrays.asList(9,9));
+		result = head.getChild(new Point(new int[] {9,9}));
 		assertNull(result);
 	}
 	
 	@Test
 	public void canFindNearest(){
-		List<List<Integer>> list = new ArrayList<List<Integer>>();
-		list.add(Arrays.asList(2, 1));
-		list.add(Arrays.asList(1, 1));
-		list.add(Arrays.asList(3, 3));
+		List<Point> list = new ArrayList<Point>();
+		list.add(new Point(new int[]{2,1}));
+		list.add(new Point(new int[]{1,1}));
+		list.add(new Point(new int[]{3,3}));
 		
-		KDTree.Node head = KDTree.build(list, 0);
+		Node head = KDTree.build(list, 0);
+		System.out.println(head.toString());
 		
-		KDTree.Node result = head.getNearest(Arrays.asList(2,1));
+		Node result = head.findClosest(null, new Point(new int[]{2,1}));
 		assertNotNull(result);
-		assertEquals(new Integer(2),result.keys.get(0));
-		assertEquals(new Integer(1),result.keys.get(1));
+		assertArrayEquals(new int[] {2,1}, result.point.values);
 		
-		result = head.getNearest(Arrays.asList(2,2));
+		result = head.findClosest(null, new Point(new int[]{2,2}));
 		assertNotNull(result);
-		assertEquals(new Integer(2),result.keys.get(0));
-		assertEquals(new Integer(1),result.keys.get(1));
+		assertArrayEquals(new int[] {2,1}, result.point.values);
 	}
 	
 	@Test
 	public void nodeGetChild(){
-		Node n = new Node(Arrays.asList(3, 2), 0);
-		n.left = new Node(Arrays.asList(2, 3), 1);
+		Node n = new Node(new Point(new int[]{3,2}), 0);
+		n.left = new Node(new Point(new int[]{2,3}), 1);
 		
-		Node lookupNode = n.getChild(Arrays.asList(2, 3));
+		Node lookupNode = n.getChild(new Point(new int[]{2,3}));
 		assertEquals(n.left, lookupNode);
 		
-		n.left.left = new Node(Arrays.asList(1,2),0);
+		n.left.left = new Node(new Point(new int[]{1,2}),0);
 		
-		lookupNode = n.getChild(Arrays.asList(1,2));
+		lookupNode = n.getChild(new Point(new int[]{1,2}));
 		assertEquals(n.left.left, lookupNode);
+	}
+	
+	@Test
+	public void squaredDistanceIsCorrect(){
+		Point a = new Point(new int[]{0,0});
+		Point b = new Point(new int[]{2,2});
+		assertEquals(8, a.squaredDistanceTo(b));
+	}
+	
+	@Test
+	public void nodeGetNearChild(){
+		List<Point> list = new ArrayList<Point>();
+		list.add(new Point(new int[]{2,1}));
+		list.add(new Point(new int[]{1,1}));
+		list.add(new Point(new int[]{3,3}));
+		
+		Node head = KDTree.build(list, 0);
+		
+		assertArrayEquals(new int[]{1,1}, head.getChildNear(new Point(new int[]{2,1})).point.values);
+		assertArrayEquals(new int[]{1,1}, head.getChildNear(new Point(new int[]{1,1})).point.values);
+		assertArrayEquals(new int[]{1,1}, head.getChildNear(new Point(new int[]{0,0})).point.values);
+		assertArrayEquals(new int[]{1,1}, head.getChildNear(new Point(new int[]{-1,-1})).point.values);
+		assertArrayEquals(new int[]{3,3}, head.getChildNear(new Point(new int[]{5,5})).point.values);
+	}
+	
+	@Test
+	public void nodeGetFarChild(){
+		List<Point> list = new ArrayList<Point>();
+		list.add(new Point(new int[]{2,1}));
+		list.add(new Point(new int[]{1,1}));
+		list.add(new Point(new int[]{3,3}));
+		
+		Node head = KDTree.build(list, 0);
+		
+		assertArrayEquals(new int[]{1,1}, head.getChildFar(new Point(new int[]{3,3})).point.values);
+		assertArrayEquals(new int[]{3,3}, head.getChildFar(new Point(new int[]{2,1})).point.values);
+		assertArrayEquals(new int[]{3,3}, head.getChildFar(new Point(new int[]{1,1})).point.values);
+		assertArrayEquals(new int[]{3,3}, head.getChildFar(new Point(new int[]{0,0})).point.values);
+		assertArrayEquals(new int[]{3,3}, head.getChildFar(new Point(new int[]{-1,-1})).point.values);
+		assertArrayEquals(new int[]{1,1}, head.getChildFar(new Point(new int[]{5,5})).point.values);
 	}
 }
