6416002e927dd0eade072a9bcd19be6899e46a70
Stephen Nancekivell <stephen.nancekivell@gmail.com>
2012-04-19 20:48:13 +1200

    nearest neigbour

--- src/main/java/info/stephenn/datastructures/KDTree.java
+++ src/main/java/info/stephenn/datastructures/KDTree.java
@@ -1,84 +1,132 @@
 package info.stephenn.datastructures;
 
+import info.stephenn.datastructures.KDTree.Node.Point;
+
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
 public class KDTree {
 	
-	public static Node build(List<List<Integer>> values, int depth){
-		if (values.isEmpty()) return null;
-		for(List<Integer> keys : values){
-			assert(keys.size() == values.size());
-		}
+	public static Node build(List<Point> points, int depth){
+		if (points.isEmpty()) return null;
 		
-		final int axis = depth % (values.get(0).size());
+		final int axis = depth % (points.get(0).values.length);
 		
-		Collections.sort(values, new Comparator<List<Integer>>() {
-			public int compare(List<Integer> o1, List<Integer> o2) {
-				return o1.get(axis).compareTo(o2.get(axis));
+		Collections.sort(points, new Comparator<Point>() {
+			public int compare(Point o1, Point o2) {
+				if (o1.values[axis] < o2.values[axis])
+					return -1;
+				else if (o1.values[axis] == o2.values[axis])
+					return 0;
+				else return 1;
 			}
 		});
 		
-		int median = values.size() / 2;
+		int median = points.size() / 2;
 		
-		Node n =  new Node(values.get(median), axis);
-		n.left = build(values.subList(0, median), depth+1);
-		n.right = build(values.subList(median+1, values.size()), depth+1);
-		return n;
+		Node node =  new Node(points.get(median), axis);
+		node.left = build(points.subList(0, median), depth+1);
+		node.right = build(points.subList(median+1, points.size()), depth+1);
+		return node;
 	}
 	
 	public static class Node {
-		final List<Integer> keys;
-		int axis =0;
+		final Point point;
+		final int axis;
 		Node left = null;
 		Node right = null;
-		public Node(List<Integer> keys, int axis){
-			this.keys = keys;
+		
+		public Node(Point point, int axis){
+			this.point = point;
 			this.axis = axis;
 		}
 		
-		public Node getChild(List<Integer> lookup) {
-			int comp = keys.get(axis).compareTo(lookup.get(axis));
-			if (comp == 0) {
+		public Node getChild(Point lookup) {
+			if (point.values[axis] < lookup.values[axis] && right != null)
+					return right.getChild(lookup);
+			else if (Arrays.equals(point.values, lookup.values))
 				return this;
-			} else if (comp < 0 && right != null) {
-				return right.getChild(lookup);
-			} else if (comp > 0 && left != null) {
+			else if ( point.values[axis] > lookup.values[axis] && left != null)
 				return left.getChild(lookup);
-			} else {
-				return null; // doesnt exist
-			}
+			else
+				return null;
 		}
 		
-		public Node getNearest(List<Integer> lookup){
-			return null;
-			//TODO
-		}
-		
-		public Node fildClosest(Node currentBest, List<Integer> toKeys){
-			if (currentBest == null) return this;
+		public Node findClosest(Node currentBest, Point toPoint){
+			if (currentBest == null) currentBest = this;
 			
+			// consider current.
+			if (point.squaredDistanceTo(toPoint) < currentBest.point.squaredDistanceTo(toPoint))
+				currentBest = this;
 			
-			return null;
+			// the near child.
+			Node nearestChild = this.getChildNear(toPoint);
+			if (nearestChild != null)
+				currentBest = nearestChild.findClosest(currentBest, toPoint);
+			
+			// the far child. maybe.
+			if (this.point.squaredAxisDistanceTo(toPoint, axis) <= currentBest.point.squaredDistanceTo(toPoint)){
+				Node childFar = this.getChildFar(toPoint);
+				if (childFar != null)
+					currentBest = childFar.findClosest(currentBest, toPoint);
+			}	
+			
+			return currentBest;
 		}
 		
-		public int squaredDistanceTo(Node n){
-			assert(n.keys.size()== this.keys.size());
-			int distance =0;
-			for (int i=0; i < n.keys.size(); i++){
-				int diff = (keys.get(i)-n.keys.get(i));
-				distance += diff * diff;
-			}
-			return distance;
+		/*
+		 * Returns the closest point. left on equal.
+		 */
+		public Node getChildNear(Point toPoint){
+			if (isRightChildNearest(toPoint))
+				return right;
+			else return left;
 		}
 		
-		public static class point{
+		private boolean isRightChildNearest(Point toPoint){
+			return point.values[axis] < toPoint.values[axis];
+		}
+		
+		public Node getChildFar(Point fromPoint){
+			if (!isRightChildNearest(fromPoint))
+				return right;
+			else return left;
+		}
+		
+		public static class Point {
 			public final int[] values;
-			public point(int[] _values){
+			public Point(int[] _values){
 				this.values = _values;
 			}
+			
+			public int squaredDistanceTo(Point p){
+				int distance = 0;
+				for (int i=0; i < values.length; i++){
+					int diff = values[i]-p.values[i];
+					distance += diff*diff;
+				}
+				return distance;
+			}
+			public int squaredAxisDistanceTo(Point p, int axis){
+				int diff = this.values[axis] - p.values[axis];
+				return diff*diff;
+			}
+		}
+
+		@Override
+		public String toString() {
+			String ret = "("+this.axis+","+ Arrays.toString(point.values);
+			if (left != null)
+				ret += left;
+			
+			ret += ",";
+			if (right != null)
+				ret += right;
+			
+			ret += ") ";
+			return ret;
 		}
 	}
-	
 }
